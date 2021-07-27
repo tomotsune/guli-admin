d86a5faec3e51498f189a4475de5177c1acc3520
tomotsune <tomotsune@foxmail.com>
2021-07-27 20:25:26 +0800

    version 2.5.5 章节添加完成

--- src/api/edu/chapter.js
+++ src/api/edu/chapter.js
@@ -7,5 +7,35 @@ export default {
       url: `/eduservice/chapter/getChapterVideo/${courseId}`,
       method: 'get',
     })
+  },
+  // 2. 添加章节
+  addChapter(chapter) {
+    return request({
+      url: `/eduservice/chapter/addChapter`,
+      method: 'post',
+      data:chapter
+    })
+  },
+  // 3. 根据id查询章节
+  getChapter(chapterId) {
+    return request({
+      url: `/eduservice/chapter/getChapterInfo/${chapterId}`,
+      method: 'get'
+    })
+  },
+  // 4. 修改章节
+  udpateChapter(chapter) {
+    return request({
+      url: `/eduservice/chapter/updateChapter`,
+      method: 'post',
+      data:chapter
+    })
+  },
+  // 5. 删除
+  deleteChapter(chapterId){
+    return request({
+      url: `/eduservice/chapter/${chapterId}`,
+      method: 'delete'
+    })
   }
 }
--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -13,6 +13,23 @@
       default-expand-all
     />
 
+    <el-button type="text" @click="openChapterDialog">添加章节</el-button>
+    <!-- 添加和修改章节表单 -->
+    <el-dialog :visible.sync="dialogChapterFormVisible" title="添加章节">
+      <el-form :model="chapter" label-width="120px">
+        <el-form-item label="章节标题">
+          <el-input v-model="chapter.title"/>
+        </el-form-item>
+        <el-form-item label="章节排序">
+          <el-input-number v-model="chapter.sort" :min="0" controls-position="right"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogChapterFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveOrUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
+
 
     <el-form label-width="120px">
       <el-form-item>
@@ -32,6 +49,8 @@ export default {
   data() {
     return {
       saveBtnDisabled: false, // 保存按钮是否禁用
+      dialogChapterFormVisible: false,
+      chapter: {},
       chapterVideo: [],
       defaultProps: {
         children: 'collection',
@@ -39,14 +58,33 @@ export default {
       }
     }
   },
-  created() {
-    this.getChapterVideo(this.$route.params.id)
+  mounted() {
+    this.chapter.courseId = this.$route.params.id
   },
   methods: {
-    // 根据课程id查询章节小节
-    getChapterVideo(id) {
-      chapter.getChapterVideo(id)
+    openChapterDialog() {
+      // 弹框
+      dialogChapterFormVisible = true
+      // 清空
+      this.chapter = {courseId: this.$route.params.id}
+    },
+    getChapterVideo() {
+      chapter.getChapterVideo(this.chapter.courseId)
         .then(res => this.chapterVideo = res.data)
+    },
+    addChapter() {
+      chapter.addChapter(this.chapter)
+        .then(res => {
+          // 关闭弹框
+          this.dialogChapterFormVisible = false
+          // 提示
+          this.$message.success('添加章节成功')
+          // 刷新页面
+          this.getChapterVideo()
+        })
+    },
+    saveOrUpdate() {
+      this.addChapter()
     },
     previous() {
       this.$router.push({path: `/edu/course/info/${this.$route.params.id}`})
